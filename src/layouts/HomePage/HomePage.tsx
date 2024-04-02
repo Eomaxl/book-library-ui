697444be01f127571500033e79311b07dbe9771c
Sourav Mansingh (somansin) <somansin@cisco.com>
2024-04-02 22:40:40 +0530

    Initial Commit

---
+++ src/layouts/HomePage/HomePage.tsx
@@ -0,0 +1,15 @@
+import {ExploreTopBooks} from "./Components/ExploreTopBooks";
+import {Carousel} from "./Components/Carousel";
+import {LibraryServices} from "./Components/LibraryServices";
+import {Heros} from "./Components/Heros";
+
+export const HomePage = () => {
+    return(
+        <>
+            <ExploreTopBooks/>
+            <Carousel/>
+            <Heros/>
+            <LibraryServices/>
+        </>
+    )
+}
